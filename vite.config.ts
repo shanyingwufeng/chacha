c394dff77f8174942a8fabfa2647ac565b7add26
xuyifan <shanyingwufeng@163.com>
2026-05-09 14:31:28 +0800

    feat: 初始化

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    // GitHub Pages (Project Pages) 部署路径：/<repo>/
+    base: "/chacha/",
+    plugins: [react()],
+});
